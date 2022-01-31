0378e7de1b6072c118a5ab5572c4170f0b8f9651
kon_yu <yusukedrive@gmail.com>
2022-01-31 17:33:25 +0900

    Add: relay settings

--- app/graphql/resolvers/links_search.rb
+++ app/graphql/resolvers/links_search.rb
@@ -5,7 +5,7 @@ class Resolvers::LinksSearch < GraphQL::Schema::Resolver
 
   scope { Link.all }
 
-  type [Types::LinkType]
+  type Types::LinkType.connection_type
 
   class LinkFilter < ::Types::BaseInputObject
     argument :OR, [self], required: false
@@ -19,8 +19,6 @@ class Resolvers::LinksSearch < GraphQL::Schema::Resolver
   end
 
   option :filter, type: LinkFilter, with: :apply_filter
-  option :first, type: types.Int, with: :apply_first
-  option :skip, type: types.Int, with: :apply_skip
   option :orderBy, type: LinkOrderBy, default: 'createdAt_DESC'
 
   def apply_filter(scope, value)
@@ -38,14 +36,6 @@ class Resolvers::LinksSearch < GraphQL::Schema::Resolver
     value[:OR].reduce(branches) { |s, v| normalize_filters(v, s) } if value[:OR].present?
 
     branches
-  end
-
-  def apply_first(scope, value)
-    scope.limit(value)
-  end
-
-  def apply_skip(scope, value)
-    scope.offset(value)
   end
 
   def apply_order_by_with_created_at_asc(scope)
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,13 @@
 module Types
   class QueryType < BaseObject
-    add_field GraphQL::Types::Relay::NodeField
-    add_field GraphQL::Types::Relay::NodesField
-
     field :all_links, resolver: Resolvers::LinksSearch
     field :_all_links_meta, QueryMetaType, null: false
+
+    field :simple_links, LinkType.connection_type, null: false
+
+    def simple_links
+      Link.all
+    end
 
     def _all_links_meta
       Link.count
